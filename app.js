b973f365e19e890353a6f99b4fdf009b3f5ec14e
Adam Zhou <adzhou218@gmail.com>
2018-09-21 12:07:38 -0400

    updated app.js

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
-const index = require("./index");
+const index = require("./views/index");
 
 const app = express();
-app.use(express.static(__dirname + "./stylesheets"));
+app.use(express.static(__dirname + "./views/stylesheets"));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.send();
 });
 
 const PORT = 3000;
